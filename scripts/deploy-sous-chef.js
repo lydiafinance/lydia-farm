8ec174d17a59df71dbaa94f38f4b3e7a26899f33
manes <manes@lydia.finance>
2021-05-04 21:57:59 +0300

    sous chef deploy script update

--- scripts/deploy-sous-chef.js
+++ scripts/deploy-sous-chef.js
@@ -7,27 +7,18 @@ async function main() {
 
   const [deployer] = await ethers.getSigners();
 
-  let SYRUP = "";
-  let REWARD_TOKEN = "";
-  let REWARD_PER_BLOCK = ""
-  let START_TS = ""
-  let BONUS_END_TS = ""
+  let SYRUP = "0x12F81569Fe25657589628B4637d0F16095d87973"; // testnet lyd
+  let REWARD_TOKEN = "0x6d3b5f42f625031304a86dfbc6bba506f6047088"; // testnet uni
+  let REWARD_PER_BLOCK = "15000000000000000"; // 0,015
+  let START_TS = "1620155100"
+  let BONUS_END_TS = "1620327900"
 
-  if (process.env.HARDHAT_NETWORK === "fuji") {
-    SYRUP = "0x12F81569Fe25657589628B4637d0F16095d87973"; // lyd
-    REWARD_TOKEN = "0x6d3b5f42f625031304a86dfbc6bba506f6047088"; // uni
-    REWARD_PER_BLOCK = "1000000000000000000";
-    START_TS = "1620055800";
-    BONUS_END_TS = "1633275000";
-  } else if (process.env.HARDHAT_NETWORK === "mainnet") {
+  if (process.env.HARDHAT_NETWORK === "mainnet") {
     SYRUP = "";
     REWARD_TOKEN = "";
     REWARD_PER_BLOCK = "";
     START_TS = "";
     BONUS_END_TS = "";
-  } else {
-    console.error("Not a valid network!");
-    process.exit(1);
   }
 
   console.log("Deploying to:", process.env.HARDHAT_NETWORK);
